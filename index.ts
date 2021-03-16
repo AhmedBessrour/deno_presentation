0c5711fd8493ef60555784f24225eac391ec82ea
Ahmed Bessrour <ahmed.bessrour@gmail.com>
2021-03-16 23:37:08 +0100

    :rocket: aws sdk
    get list of dynamodb tables

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Welcome to Deno!");
+import { DynamoDBClient, ListTablesCommand } from "https://deno.land/x/aws_sdk@v3.8.0.0/client-dynamodb/mod.ts";
+
+
+(async () => {
+  const client = new DynamoDBClient({ region: "eu-west-3" });
+  const command = new ListTablesCommand({});
+  try {
+    const results = await client.send(command);
+    console.log(results.TableNames);
+  } catch (err) {
+    console.error(err);
+  }
+})();
